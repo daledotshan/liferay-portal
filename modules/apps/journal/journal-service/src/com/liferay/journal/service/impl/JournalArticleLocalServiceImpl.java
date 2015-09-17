6b9717b13c13a609774447e72ae1d0b2f16d617f
hai.yu <8219754@liferay.com>
2015-09-17 13:53:37 +0800

    LPS-58438 Exclude import process, otherwise, it will cause import performance issue. We can use verify process to modify the old datas.

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -5768,7 +5768,8 @@ public class JournalArticleLocalServiceImpl
 		if ((status == WorkflowConstants.STATUS_APPROVED) &&
 			(article.getExpirationDate() != null) &&
 			(JournalServiceConfigurationValues.
-				JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS)) {
+				JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS) &&
+			!ExportImportThreadLocal.isImportInProcess()) {
 
 			List<JournalArticle> currentArticles =
 				journalArticlePersistence.findByG_A(
