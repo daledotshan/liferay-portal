06e1f0f61204c2c75caa24c3696e2cf227e63de6
sherlyliu <sherly.liu@liferay.com>
2016-06-28 14:26:41 +0800

    LPS-66857 Birthday cannot be set to a date in the future.

--- portal-impl/src/com/liferay/portal/service/impl/ContactLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ContactLocalServiceImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portal.service.impl;
 import com.liferay.portal.kernel.exception.ContactBirthdayException;
 import com.liferay.portal.kernel.exception.ContactClassNameException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.Contact;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
@@ -48,6 +49,11 @@ public class ContactLocalServiceImpl extends ContactLocalServiceBaseImpl {
 		Date birthday = PortalUtil.getDate(
 			birthdayMonth, birthdayDay, birthdayYear,
 			ContactBirthdayException.class);
+		Date now = new Date();
+
+		if (birthday != null && birthday.after(now)) {
+			throw new SystemException("Birthday can be set to a future date.");
+		}
 
 		validate(className, classPK);
 
@@ -154,6 +160,11 @@ public class ContactLocalServiceImpl extends ContactLocalServiceBaseImpl {
 		Date birthday = PortalUtil.getDate(
 			birthdayMonth, birthdayDay, birthdayYear,
 			ContactBirthdayException.class);
+		Date now = new Date();
+
+		if (birthday != null && birthday.after(now)) {
+			throw new SystemException("Birthday can be set to a future date.");
+		}
 
 		Contact contact = contactPersistence.findByPrimaryKey(contactId);
 
